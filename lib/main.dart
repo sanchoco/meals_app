723fa0c5b27ecae4bba4bc6fb7c98dc4b34094a8
sanchoco <jguuun@gmail.com>
2022-07-08 21:55:17 +0900

    add font, grid menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,23 +9,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meals App',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('DeliMeals')),
-      body: Center(child: Text('Navigation Time!')),
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 220, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold),
+            ),
+      ),
+      home: CategoriesScreen(),
     );
   }
 }
