086b96850d52883e97073e268f4c60a70beb6c68
sanchoco <jguuun@gmail.com>
2022-07-14 19:55:52 +0900

    fix dummy data & add meals list

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  // final String categoryId;
-  // final String categoryTitle;
-
-  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
   static const routeName = '/category-meals';
 
   @override
@@ -12,11 +9,18 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final String? categoryId = routeArgs['id'];
     final String? categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    });
     return Scaffold(
-      appBar: AppBar(
-        title: Text(categoryTitle!),
-      ),
-      body: Center(child: Text('The Recipes For The Category!')),
-    );
+        appBar: AppBar(
+          title: Text(categoryTitle!),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals.toList()[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }
