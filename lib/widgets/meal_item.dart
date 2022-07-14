5a72e3f80092b5be1d9f4c2ad1c338e02c2352e4
sanchoco <jguuun@gmail.com>
2022-07-14 20:16:28 +0900

    change dir structure

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MealItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
